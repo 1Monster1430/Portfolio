216e1f175987c111a9354bba7a8e2fcbdf554199
1Monster1430 <gingcasagan@gmail.com>
2024-09-29 10:15:16 +0800

    1st commit
    Date:29/9/2024

---
+++ ffungtion.js
@@ -0,0 +1,4 @@
+document.getElementById('play-button').addEventListener('click', function() {
+    // Open the Tetris game in a new tab (replace with the actual URL if hosted online)
+    window.location.href = 'C:/path/to/your/tetris-game.exe'; // or use a URL like 'https://yourwebsite.com/tetris-game'
+});
